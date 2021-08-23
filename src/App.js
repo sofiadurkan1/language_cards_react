120b689b9ff34562910c73ab391549ff193ddd52
sofia durkan <sofiadurkan@gmail.com>
2021-08-22 23:47:05 -0500

    header image

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ function App() {
   return (
     <div className="App">
       <ReactLogo id="img"/>
-      <h2>Languages</h2>
       <div className="card-container">
       {categories.map((card) => (
          <Card options = {card}/>
